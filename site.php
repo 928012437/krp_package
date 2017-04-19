7b7211d56657943ecd44eaf2d665d5b0c84ae36e
chong <928012437@qq.com>
2017-04-19 15:39:52 +0800

    20170419

---
+++ site.php
@@ -0,0 +1,33 @@
+<?php
+
+class Krp_packageModuleSite extends WeModuleSite {
+
+    public function doWebMysetting() {
+        echo 'mysetting';
+    }
+
+    public function doWebGoods() {
+        echo 'mysetting';
+    }
+
+    public function doWebUserlist() {
+        echo 'mysetting';
+    }
+
+    public function doWebWinlist() {
+        echo 'mysetting';
+    }
+
+    public function doMobileIndex() {
+        echo 'index';
+    }
+
+    public function doMobilePlay() {
+        echo 'index';
+    }
+
+    public function doMobileWinlist() {
+        echo 'index';
+    }
+
+}
